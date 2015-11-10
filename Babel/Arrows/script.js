c984556182c21fa9058f9f69ca6c361a065ce8bb
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-11-10 09:53:51 +0300

    babel examples

---
+++ Babel/Arrows/script.js
@@ -0,0 +1,32 @@
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 10.11.2015
+ * Time: 9:34
+ */
+
+// Expression bodies
+var odds = evens.map(v => v + 1);
+var nums = evens.map((v, i) => v + i);
+var pairs = evens.map(v => ({even: v, odd: v + 1}));
+
+// Statement bodies
+nums.forEach(v => {
+    if (v % 5 === 0)
+    fives.push(v);
+});
+
+// Lexical this
+var bob = {
+    _name: "Bob",
+    _friends: [],
+    printFriends() {
+    this._friends.forEach(f =>
+    console.log(this._name + " knows " + f));
+}
+}
+
+
---
+++ Babel/TemplateStrings/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 10.11.2015
+ * Time: 9:44
+ */
