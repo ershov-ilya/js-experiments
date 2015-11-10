98418d816791f3d4f3476903b49198c1581e0b13
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-11-10 09:54:23 +0300

    babel eamples

--- Babel/TemplateStrings/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by PhpStorm.
- * Author:   ershov-ilya
- * GitHub:   https://github.com/ershov-ilya/
- * About me: http://about.me/ershov.ilya (EN)
- * Website:  http://ershov.pw/ (RU)
- * Date: 10.11.2015
- * Time: 9:44
- */
